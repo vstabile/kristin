ec73c41042a8faff98cb8fec2e074c139762ec48
Richard Nyström <ricny046@gmail.com>
2013-03-26 22:43:19 +0100

    Added file_path helper and requires the actual lib

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+require 'kristin'
+
+def file_path( *paths )
+  File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', *paths))
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
