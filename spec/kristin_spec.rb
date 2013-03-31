257dd5cf072541f11c244a43245cee908dca11f9
Richard Nyström <ricny046@gmail.com>
2013-03-31 15:26:40 +0200

    Added more specs to ensure the libary is robust to errors

--- spec/kristin_spec.rb
+++ spec/kristin_spec.rb
@@ -31,14 +31,24 @@ describe Kristin do
       File.exists?(target).should == true
     end
 
+    it "should raise error if pdf is not a real pdf" do
+      lambda { Kristin.convert(@no_pdf, "nonsense.html") }.should raise_error(IOError)
+    end
+
     it "should convert a pdf from an URL" do
       target = @target_path + "/from_url.html"
       Kristin.convert("https://www.filepicker.io/api/file/vR0btUfRQiCF9ntRkW6Q", target)
       File.exists?(target).should == true
     end
 
-    it "should raise error if pdf is not a real pdf" do
-      lambda { Kristin.convert(@no_pdf, "nonsense.html") }.should raise_error(IOError)
+    it "should raise an error if URL does not exist" do
+      target = @target_path + "/from_url.html"
+      lambda { Kristin.convert("https://www.filepicker.io/api/file/donotexist.pdf", target) }.should raise_error(IOError)
+    end
+
+    it "should raise an error if URL file is not a real pdf" do
+      target = @target_path + "/from_url.html"
+      lambda { Kristin.convert("https://www.filepicker.io/api/file/agxKeTfQSWKvMR4CDXMq", target) }.should raise_error(IOError)
     end
   end
 end
