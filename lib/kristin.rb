9f17f0bca3beb48b6ad234227c491a79f7f3e030
Richard Nyström <ricny046@gmail.com>
2013-04-02 23:38:43 +0200

    Refactored code and specs to make room for more advanced features

--- lib/kristin.rb
+++ lib/kristin.rb
@@ -3,60 +3,70 @@ require 'open-uri'
 require "net/http"
 
 module Kristin
+  class Converter
+    def initialize(source, target, options = {})
+      @source = source
+      @target = target
+    end
+
+    def convert
+      raise IOError, "Can't find pdf2htmlex executable in PATH" if not command_available?
+      src = determine_source(@source)
+      cmd = "#{pdf2htmlex_command} #{src} #{@target}"
+      pid = Process.spawn(cmd, [:out, :err] => "/dev/null")
+      Process.waitpid(pid)
+      ## TODO: Grab error message from pdf2htmlex and raise a better error
+      raise IOError, "Could not convert #{src}" if $?.exitstatus != 0
+    end
+
+    private
+
+    def command_available?
+      pdf2htmlex_command
+    end
+
+    def pdf2htmlex_command
+      cmd = nil
+      cmd = "pdf2htmlex" if which("pdf2htmlex")
+      cmd = "pdf2htmlEX" if which("pdf2htmlEX")
+    end
+
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+        ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+          exts.each do |ext|
+            exe = File.join(path, "#{cmd}#{ext}")
+            return exe if File.executable? exe
+          end
+        end
+      return nil
+    end
+
+    def random_source_name
+      rand(16**16).to_s(16)
+    end
+
+    def download_file(source)
+      tmp_file = "/tmp/#{random_source_name}.pdf"
+      File.open(tmp_file, "wb") do |saved_file|
+        open(source, 'rb') do |read_file|
+          saved_file.write(read_file.read)
+        end
+      end
+
+      tmp_file
+    end
+
+    def determine_source(source)
+      is_file = File.exists?(source) && !File.directory?(source)
+      is_http = (URI(source).scheme == "http" || URI(source).scheme == "https") && Net::HTTP.get_response(URI(source)).is_a?(Net::HTTPSuccess)
+      raise IOError, "Source (#{source}) is neither a file nor an URL." unless is_file || is_http
+    
+      is_file ? source : download_file(source)
+    end
+  end
+
   def self.convert(source, target)
-    raise IOError, "Can't find pdf2htmlex executable in PATH" if not command_available?
-    src = determine_source(source)
-    cmd = "#{pdf2htmlex_command} #{src} #{target}"
-    pid = Process.spawn(cmd, [:out, :err] => "/dev/null")
-    Process.waitpid(pid)
-    ## TODO: Grab error message from pdf2htmlex and raise a better error
-    raise IOError, "Could not convert #{src}" if $?.exitstatus != 0
-  end
-
-  private
-
-  def self.command_available?
-    pdf2htmlex_command
-  end
-
-  def self.pdf2htmlex_command
-    cmd = nil
-    cmd = "pdf2htmlex" if which("pdf2htmlex")
-    cmd = "pdf2htmlEX" if which("pdf2htmlEX")
-  end
-
-  def self.which(cmd)
-    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each do |ext|
-        exe = File.join(path, "#{cmd}#{ext}")
-        return exe if File.executable? exe
-      end
-    end
-    
-    return nil
-  end
-
-  def self.random_source_name
-    rand(16**16).to_s(16)
-  end
-
-  def self.download_file(source)
-    tmp_file = "/tmp/#{random_source_name}.pdf"
-    File.open(tmp_file, "wb") do |saved_file|
-      open(source, 'rb') do |read_file|
-        saved_file.write(read_file.read)
-      end
-    end
-
-    tmp_file
-  end
-
-  def self.determine_source(source)
-    is_file = File.exists?(source) && !File.directory?(source)
-    is_http = (URI(source).scheme == "http" || URI(source).scheme == "https") && Net::HTTP.get_response(URI(source)).is_a?(Net::HTTPSuccess)
-    raise IOError, "Source (#{source}) is neither a file nor an URL." unless is_file || is_http
-    
-    is_file ? source : download_file(source)
+    Converter.new(source, target).convert
   end
 end
--- spec/kristin_spec.rb
+++ spec/kristin_spec.rb
@@ -7,48 +7,74 @@ describe Kristin do
     @multi_page_pdf = file_path("multi.pdf")
     @no_pdf = file_path("image.png")
     @target_path = "tmp/kristin"
+  end
+
+  before(:each) do
     FileUtils.mkdir_p @target_path
   end
 
-  after(:all) do
+  after(:each) do
     FileUtils.rm_rf @target_path
   end
 
-  describe ".convert" do
-    it "should raise error if source file does not exists" do
-      lambda { Kristin.convert("nonsense.pdf", "nonsense.html") }.should raise_error(IOError)
+  describe "#convert" do
+    describe "with no options" do
+      it "should raise error if source file does not exists" do
+        c = Kristin::Converter.new("nonsense.pdf", "nonsense.html")
+        lambda { c.convert }.should raise_error(IOError)
+      end
+
+      it "should convert a one page pdf to one html file" do
+        target = @target_path + "/one.html"
+        Kristin::Converter.new(@one_page_pdf, target).convert
+        File.exists?(target).should == true
+      end
+
+      it "should convert a multi page pdf to one html file" do
+        target = @target_path + "/multi.html"
+        Kristin::Converter.new(@multi_page_pdf, target).convert
+        File.exists?(target).should == true
+      end
+
+      it "should raise error if pdf is not a real pdf" do
+        lambda { Kristin::Converter.new(@no_pdf, "nonsense.html").convert }.should raise_error(IOError)
+      end
+
+      it "should convert a pdf from an URL" do
+        target = @target_path + "/from_url.html"
+        Kristin::Converter.new("https://www.filepicker.io/api/file/vR0btUfRQiCF9ntRkW6Q", target).convert
+        File.exists?(target).should == true
+      end
+
+      it "should raise an error if URL does not exist" do
+        target = @target_path + "/from_url.html"
+        lambda { Kristin::Converter.new("https://www.filepicker.io/api/file/donotexist.pdf", target).convert }.should raise_error(IOError)
+      end
+
+      it "should raise an error if URL file is not a real pdf" do
+        target = @target_path + "/from_url.html"
+        lambda { Kristin::Converter.new("https://www.filepicker.io/api/file/agxKeTfQSWKvMR4CDXMq", target).convert }.should raise_error(IOError)
+      end
     end
 
-    it "should convert a one page pdf to one html file" do
+    describe "with options" do
+      it "should allow specifying first page" do
+        target = @target_path + "/multi.html"
+        Kristin::Converter.new(@one_page_pdf, target).convert
+        File.exists?(target).should == true
+      end
+    end
+  end
+
+  describe ".convert" do
+    it "should convert without options" do
       target = @target_path + "/one.html"
       Kristin.convert(@one_page_pdf, target)
       File.exists?(target).should == true
     end
 
-    it "should convert a multi page pdf to one html file" do
-      target = @target_path + "/multi.html"
-      Kristin.convert(@multi_page_pdf, target)
-      File.exists?(target).should == true
-    end
-
-    it "should raise error if pdf is not a real pdf" do
-      lambda { Kristin.convert(@no_pdf, "nonsense.html") }.should raise_error(IOError)
-    end
-
-    it "should convert a pdf from an URL" do
-      target = @target_path + "/from_url.html"
-      Kristin.convert("https://www.filepicker.io/api/file/vR0btUfRQiCF9ntRkW6Q", target)
-      File.exists?(target).should == true
-    end
-
-    it "should raise an error if URL does not exist" do
-      target = @target_path + "/from_url.html"
-      lambda { Kristin.convert("https://www.filepicker.io/api/file/donotexist.pdf", target) }.should raise_error(IOError)
-    end
-
-    it "should raise an error if URL file is not a real pdf" do
-      target = @target_path + "/from_url.html"
-      lambda { Kristin.convert("https://www.filepicker.io/api/file/agxKeTfQSWKvMR4CDXMq", target) }.should raise_error(IOError)
+    it "should convert with options" do
+      pending
     end
   end
 end
