ba00547558bf380160620394b6214b67f6ba6707
Richard Nyström <ricny046@gmail.com>
2013-04-04 00:40:20 +0200

    Cleaned up the specs

--- spec/kristin_spec.rb
+++ spec/kristin_spec.rb
@@ -10,6 +10,7 @@ describe Kristin do
     @no_pdf = file_path("image.png")
     @large_pdf = file_path("large.pdf")
     @target_path = "tmp/kristin"
+    @target_file = @target_path + "/output.html"
     @fast_opts = { process_outline: false, vdpi: 1, hdpi: 1, first_page: 1, last_page: 1 }
     dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))  
     port = 50510
@@ -40,15 +41,13 @@ describe Kristin do
       end
 
       it "should convert a one page pdf to one html file" do
-        target = @target_path + "/one.html"
-        Kristin::Converter.new(@one_page_pdf, target, @fast_opts).convert
-        File.exists?(target).should == true
+        Kristin::Converter.new(@one_page_pdf, @target_file, @fast_opts).convert
+        File.exists?(@target_file).should == true
       end
 
       it "should convert a multi page pdf to one html file" do
-        target = @target_path + "/multi.html"
-        Kristin::Converter.new(@multi_page_pdf, target, @fast_opts).convert
-        File.exists?(target).should == true
+        Kristin::Converter.new(@multi_page_pdf, @target_file, @fast_opts).convert
+        File.exists?(@target_file).should == true
       end
 
       it "should raise error if pdf is not a real pdf" do
@@ -56,19 +55,16 @@ describe Kristin do
       end
 
       it "should convert a pdf from an URL" do
-        target = @target_path + "/from_url.html"
-        Kristin::Converter.new("#{@url}/one.pdf", target, @fast_opts).convert
-        File.exists?(target).should == true
+        Kristin::Converter.new("#{@url}/one.pdf", @target_file, @fast_opts).convert
+        File.exists?(@target_file).should == true
       end
 
       it "should raise an error if URL does not exist" do
-        target = @target_path + "/from_url.html"
-        lambda { Kristin::Converter.new("#{@url}/donotexist.pdf", target).convert }.should raise_error(IOError)
+        lambda { Kristin::Converter.new("#{@url}/donotexist.pdf", @target_file).convert }.should raise_error(IOError)
       end
 
       it "should raise an error if URL file is not a real pdf" do
-        target = @target_path + "/from_url.html"
-        lambda { Kristin::Converter.new("#{@url}/image.png", target).convert }.should raise_error(IOError)
+        lambda { Kristin::Converter.new("#{@url}/image.png", @target_file).convert }.should raise_error(IOError)
       end
     end
 
@@ -76,17 +72,15 @@ describe Kristin do
       #TODO: Only convert file once for performance
       
       it "should process outline by default" do
-        target = @target_path + "/large.html"
-        Kristin::Converter.new(@large_pdf, target, { process_outline: false }).convert
-        doc = Nokogiri::HTML(File.open(target))
+        Kristin::Converter.new(@large_pdf, @target_file, { process_outline: false }).convert
+        doc = Nokogiri::HTML(File.open(@target_file))
         el = doc.css("#pdf-outline").first
         el.children.should_not be_empty
       end
 
       it "should be possible to disable outline" do
-        target = @target_path + "/large.html"
-        Kristin::Converter.new(@large_pdf, target, { process_outline: false }).convert
-        doc = Nokogiri::HTML(File.open(target))
+        Kristin::Converter.new(@large_pdf, @target_file, { process_outline: false }).convert
+        doc = Nokogiri::HTML(File.open(@target_file))
         el = doc.css("#pdf-outline").first
         el.children.first.text.strip.should be_empty
       end
@@ -104,9 +98,8 @@ describe Kristin do
       end
 
       it "should be possible to specify last page" do
-        target = @target_path + "/multi.html"
-        Kristin::Converter.new(@multi_page_pdf, target, { last_page: 9 }).convert
-        doc = Nokogiri::HTML(File.open(target))
+        Kristin::Converter.new(@multi_page_pdf, @target_file, { last_page: 9 }).convert
+        doc = Nokogiri::HTML(File.open(@target_file))
         # Content only present on page 1
         content_from_page_1 = doc.search("//span").map(&:content).select {|c| c.include? "Geometric series"}
         # Content only present on page 10
@@ -116,9 +109,8 @@ describe Kristin do
       end
 
       it "should be possible to specify hdpi and vdpi" do
-        target = @target_path + "/one.html"
-        Kristin::Converter.new(@one_page_pdf, target, { hdpi: 5, vdpi: 5 }).convert
-        doc = Nokogiri::HTML(File.open(target))
+        Kristin::Converter.new(@one_page_pdf, @target_file, { hdpi: 5, vdpi: 5 }).convert
+        doc = Nokogiri::HTML(File.open(@target_file))
         doc.xpath("//img/@src").first.content.size.should == 4938 # The size you get when hdpi and vdpi is 5 on @one_page_pdf
       end
     end
@@ -126,15 +118,13 @@ describe Kristin do
 
   describe ".convert" do
     it "should convert without options" do
-      target = @target_path + "/one.html"
-      Kristin.convert(@one_page_pdf, target)
-      File.exists?(target).should == true
+      Kristin.convert(@one_page_pdf, @target_file)
+      File.exists?(@target_file).should == true
     end
 
     it "should convert with options" do
-      target = @target_path + "/one.html"
-      Kristin.convert(@one_page_pdf, target, { hdpi: 1, vdpi: 1 })
-      File.exists?(target).should == true
+      Kristin.convert(@one_page_pdf, @target_file, { hdpi: 1, vdpi: 1 })
+      File.exists?(@target_file).should == true
     end
   end
 end
